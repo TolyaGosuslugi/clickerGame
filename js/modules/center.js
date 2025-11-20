09b853dceee995b6c216b7232fae753280269ce0
Nikita Belov <nikita2010belov@gmail.com>
2025-11-20 15:04:40 +0500

    Add files via upload

--- js/modules/center.js
+++ js/modules/center.js
@@ -4,18 +4,21 @@ const mainClicks = document.getElementById('mainClicks')
 const mainSpeed = document.getElementById('mainSpeed')
 const mainBlock = document.getElementById('mainBlock')
 
-let clicks = 0
+let clicks = localStorage.getItem("clicks") != null ? +localStorage.getItem("clicks") : 0;
 let clicksPerSec = 0
 let clicksTime = []
+
+mainClicks.innerHTML = clicks;
 
 mainBlock.addEventListener(('click'), () => {
     clicks += 1
     mainClicks.innerHTML = clicks
+    localStorage.clicks = clicks; //save to local
     clicksTime.push(Date.now())
 })
 
-function clearClicksTime(){
-    while(clicksTime.length != 0 && clicksTime[0] < Date.now() - 1000){
+function clearClicksTime() {
+    while (clicksTime.length != 0 && clicksTime[0] < Date.now() - 1000) {
         clicksTime.shift()
     }
     mainSpeed.innerHTML = clicksTime.length
